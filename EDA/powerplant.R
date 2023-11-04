934c23a665d393344a39d0a74baa85a524bb02b6
annihong <annihong7153@gmail.com>
2023-11-04 13:14:14 -0400

    update

--- EDA/powerplant.R
+++ EDA/powerplant.R
@@ -6,13 +6,14 @@ factory_coord <- as.data.frame(dat[,c("gppd_idnr", "latitude", "longitude")])
 
 pt_interest <- c(1,1) # 
 
-
+# returns a dataframe with factories within dist_in_m meters of the point of interest (lat, long)
 num_factory_within_dist <- function(pt_interest, factory_coord, dist_in_m) {
   res <- get_dist_to_factory(pt_interest, factory_coord)
   filtered <- res %>% filter(dist_to_pt < dist_in_m)
   return(filtered)
 }
 
+# returns a dataframe with the distance to the point of interest (lat, long) for each factory
 get_dist_to_factory <- function(pt_interest, factory_coord) {
   res <- sapply(1:nrow(factory_coord), function(i) distm(factory_coord[i,c(2,3)], pt_interest, fun = distHaversine))
   factory_coord$dist_to_pt <- res

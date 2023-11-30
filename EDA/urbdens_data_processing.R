cdf2a604da6b47845946522221e46a237e1d817f
Maya Shen <mayashen@cmu.edu>
2023-11-30 12:28:13 -0500

    Urban density data proprocessing R code (renamed)

--- EDA/urbdens_data_processing.R
+++ EDA/urbdens_data_processing.R
@@ -1,28 +1,35 @@
-# Setup ----
-# Load libraries
+#'##########################################################################
+## Code for processing urban density data from NASA's Socioeconomic Data and 
+## Applications Center (SEDAC) for Brazil
+## mayashen@cmu.edu - Nov 2023
+## NOTES
+#'##########################################################################
+
+# SECTION 0: LOAD LIBRARIES, SET DIRECTORY, ETC ----
+## Load libraries ----
 library(ncdf4)
 library(dplyr)
 library(reshape)
 library(purrr)
 library(ggplot2)
 
-# Optionally: Set own colors
+## Set working directory ----
+setwd('/Users/mayashen/Desktop/un_datathon_2023')
+ 
+## Optional: Set own colors/palette ----
 library(RColorBrewer) 
 palette <- brewer.pal(6,"YlOrRd")
 
-# Set working directory
-setwd('/Users/mayashen/Desktop/un_datathon_2023')
-
-# Load data ----
+# SECTION 1: LOAD DATA ----
 # Data downloaded from: https://sedac.ciesin.columbia.edu/data/set/gpw-v4-population-density-rev11/data-download
 
-# Define paths
-datapath <- 'DATA/gpw-v4-population-density-rev11_totpop_2pt5_min_nc/'
-savepath <- 'DATA/'
+## Define load/save data paths ----
+load_datapath <- 'DATA/gpw-v4-population-density-rev11_totpop_2pt5_min_nc/'
+save_datapath <- 'DATA/'
 
-# Load nc dataset ----
+## Load nc dataset ----
 nc_fname <- 'gpw_v4_population_density_rev11_2pt5_min.nc'
-ncin <- nc_open(paste0(datapath, nc_fname))
+ncin <- nc_open(paste0(load_datapath, nc_fname))
 names(ncin$var)
 
 lon <- ncvar_get(ncin,"longitude")
@@ -32,13 +39,15 @@ dim(lon)
 dim(lat)
 dim(popdens)
 
-# Investigate 5 slices ----
+# SECTION 2: DATA INVESTIGATION ----
+## Investigate 5 slices ----
 for (slice in 1:5) {
   print(sum(!(is.na(popdens[,,slice]))))
   print(unique(popdens[,,slice][!is.na(popdens[,,slice])])[1:10])
 }
 
-# Check that fifth slice is the most recent (2020)...?
+# Check that fifth slice is the most recent ----
+# We want the most recent (2020) slice
 t0 <- Sys.time()
 for (slice in 1:5) {
   popdensyr <- popdens[,,slice]
@@ -122,12 +131,12 @@ for (slice in 1:5) {
 t1 <- Sys.time()
 print(t1-t0)
 
-# Get fifth slice ----
+## Get fifth slice ----
 slice <- 5
 popdensyr <- popdens[,,slice]
 
-# Subset points 
-# Get points within bounding box of Brazil
+# SECTION 3: BOUNDING BOX ----
+## Get points within bounding box of Brazil ----
 # lat = [-35, 5], lon = [-75, -35]
 lon_bbox_bool <- between(lon, -75, -35)
 lat_bbox_bool <- between(lat, -35, 5)
@@ -138,21 +147,32 @@ popdensyr_bbox <- popdensyr[lon_bbox_bool, lat_bbox_bool]
 
 # Melt matrix into dataframe
 popdensyr_bbox_df <- melt(popdensyr_bbox)
+
 # Rename columns
 popdensyr_bbox_df$latitude <- lat_bbox[popdensyr_bbox_df$Var2]
 popdensyr_bbox_df$longitude <- lon_bbox[popdensyr_bbox_df$Var1]
 popdensyr_bbox_df$popdens <- popdensyr_bbox_df$value
 popdensyr_bbox_df <- popdensyr_bbox_df[, c('latitude', 'longitude', 'popdens')]
 
-# Remove points with NA urban density values
+## Remove NA urban density points ----
 popdensyr_bbox_df <- popdensyr_bbox_df[!is.na(popdensyr_bbox_df$popdens),]
 nrow(popdensyr_bbox_df) # 676778 points
 
-# Plot population density (log scale)
+# SECTION 4: PLOTTING ----
+
+## Define save figure path ----
+save_figpath <- 'DATA/urbdens_plots/'
+  
+## Plot population density (log scale) ----
 ggplot(data=popdensyr_bbox_df, aes(x=longitude, y=latitude, col=log(popdens))) +
   geom_point(size=0.2, alpha=0.5)
+# ggsave(paste0(save_figpath, slice, '_logplot.png'),
+#        width=7.25,
+#        height=6.25,
+#        units='in',
+#        dpi=300)
 
-# Plot population density grouped according to web plots
+## Plot population density grouped according to web plots ----
 # https://sedac.ciesin.columbia.edu/data/set/gpw-v4-population-density-rev11/maps
 
 cat_popdens_mx <- matrix(NA, nrow=length(popdensyr_bbox_df$popdens), ncol=6)
@@ -164,7 +184,6 @@ for (cat_i in 1:6) {
 }
 # No population density values at the boundaries
 unique(rowSums(cat_popdens_mx))
-
 
 cat_popdens_df <- melt(cat_popdens_mx)
 cat_popdens_df <- cat_popdens_df[cat_popdens_df$value,]
@@ -188,9 +207,6 @@ cat_popdens <- unlist(map(cat_popdens_df$X2, popdens_cat_mapfn))
 
 popdensyr_bbox_df$popdens_cat <- factor(cat_popdens, levels=c('<1', '1-5', '5-25', '25-250', 
                                                               '250-1,000', '>1,000'))
-# Optionally: Set own colors
-library(RColorBrewer) 
-palette <- brewer.pal(6,"YlOrRd")
 
 ggplot(data=popdensyr_bbox_df, aes(x=longitude, y=latitude, col=popdens_cat)) +
   geom_point(size=0.2, alpha=0.5) +
@@ -200,13 +216,13 @@ ggplot(data=popdensyr_bbox_df, aes(x=longitude, y=latitude, col=popdens_cat)) +
                                 '25-250' = palette[4], 
                                 '250-1,000' = palette[5], 
                                 '>1,000' = palette[6])) 
-ggsave(paste0('DATA/popdens_plots/', slice, '_catplot.png'),
-       width=7.25,
-       height=6.25,
-       units='in',
-       dpi=300)
+# ggsave(paste0(save_figpath, slice, '_catplot.png'),
+#        width=7.25,
+#        height=6.25,
+#        units='in',
+#        dpi=300)
 
-# Save dataframe ----
-write.csv(popdensyr_bbox_df, paste0(savepath, 'urbDens_data.csv'), row.names=TRUE)
+# SECTION 4: SAVE BRAZIL URBAN DENSITY DATA ----
+write.csv(popdensyr_bbox_df, paste0(save_datapath, 'urbDens_data.csv'), row.names=TRUE)
 
-popdensyr_bbox_df <- read.csv(paste0(savepath, 'urbDens_data.csv'), row.names=1)
+# popdensyr_bbox_df <- read.csv(paste0(save_datapath, 'urbDens_data.csv'), row.names=1)
